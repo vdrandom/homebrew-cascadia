bd347ff966c171ee062a2481b98e2bf431b3de1f
Von Random <von@mechanus.net>
2024-12-30 05:19:07 +0200

    initial commit

---
+++ otf-cascadia-mono-pl.rb
@@ -0,0 +1,40 @@
+cask "otf-cascadia-mono-pl" do
+  version "2407.24"
+  sha256 "e67a68ee3386db63f48b9054bd196ea752bc6a4ebb4df35adce6733da50c8474"
+
+  url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
+  name "Cascadia Mono PL"
+  homepage "https://github.com/microsoft/cascadia-code"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  font "otf/static/CascadiaCodePL-Bold.otf"
+  font "otf/static/CascadiaCodePL-BoldItalic.otf"
+  font "otf/static/CascadiaCodePL-ExtraLight.otf"
+  font "otf/static/CascadiaCodePL-ExtraLightItalic.otf"
+  font "otf/static/CascadiaCodePL-Italic.otf"
+  font "otf/static/CascadiaCodePL-Light.otf"
+  font "otf/static/CascadiaCodePL-LightItalic.otf"
+  font "otf/static/CascadiaCodePL-Regular.otf"
+  font "otf/static/CascadiaCodePL-SemiBold.otf"
+  font "otf/static/CascadiaCodePL-SemiBoldItalic.otf"
+  font "otf/static/CascadiaCodePL-SemiLight.otf"
+  font "otf/static/CascadiaCodePL-SemiLightItalic.otf"
+  font "otf/static/CascadiaMonoPL-Bold.otf"
+  font "otf/static/CascadiaMonoPL-BoldItalic.otf"
+  font "otf/static/CascadiaMonoPL-ExtraLight.otf"
+  font "otf/static/CascadiaMonoPL-ExtraLightItalic.otf"
+  font "otf/static/CascadiaMonoPL-Italic.otf"
+  font "otf/static/CascadiaMonoPL-Light.otf"
+  font "otf/static/CascadiaMonoPL-LightItalic.otf"
+  font "otf/static/CascadiaMonoPL-Regular.otf"
+  font "otf/static/CascadiaMonoPL-SemiBold.otf"
+  font "otf/static/CascadiaMonoPL-SemiBoldItalic.otf"
+  font "otf/static/CascadiaMonoPL-SemiLight.otf"
+  font "otf/static/CascadiaMonoPL-SemiLightItalic.otf"
+
+  # No zap stanza required
+end
